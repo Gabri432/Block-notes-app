4f654d7038d7fb40e277fffe90bf1c61a03f3deb
Gabri432 <gabrielegatti432@gmail.com>
2022-08-07 17:56:36 +0200

    Creating the 'internal_functions.go' to store some functions used 'block_notes.go'.

--- block_notes.go
+++ block_notes.go
@@ -21,6 +21,11 @@ type Post struct {
 
 type Posts []Post
 
+type Data struct {
+	Content  Post
+	ReadMode bool
+}
+
 func main() {
 	http.HandleFunc("/", readPost)
 	http.HandleFunc("/drafts", readPost)
@@ -33,7 +38,8 @@ func main() {
 func readPost(w http.ResponseWriter, r *http.Request) {
 	content, err := ioutil.ReadFile("database/posts.json")
 	if err != nil {
-		respondError(w, http.StatusInternalServerError, err.Error())
+		RespondError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 	var posts Posts
 	var drafts Posts
@@ -43,7 +49,8 @@ func readPost(w http.ResponseWriter, r *http.Request) {
 	}
 	htmlPage, err := template.ParseFiles("templates/main.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
-		respondError(w, http.StatusInternalServerError, err.Error())
+		RespondError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 	if len(drafts) > 0 {
 		htmlPage.Execute(w, drafts)
@@ -62,15 +69,16 @@ func getDrafts(posts Posts) (drafts Posts) {
 }
 
 func createPost(w http.ResponseWriter, r *http.Request) {
+	RenderHTML(w, "templates/form.html", false, Post{})
 	if http.MethodPost != r.Method {
 		return
 	}
-	renderHTML(w, "templates/form.html", false)
 	post := getFormData(w, r)
 	if post.Title == "" {
-		respondError(w, http.StatusNoContent, "Not title provided.")
+		RespondError(w, http.StatusNoContent, "Not title provided.")
+		return
 	}
-	savePost(w, post, "database/posts.json")
+	SavePost(w, post, "database/posts.json")
 }
 
 func getFormData(w http.ResponseWriter, r *http.Request) Post {
@@ -83,78 +91,46 @@ func getFormData(w http.ResponseWriter, r *http.Request) Post {
 	}
 }
 func updatePost(w http.ResponseWriter, r *http.Request) {
-	renderHTML(w, "templates/form.html", false)
 	content, err := ioutil.ReadFile("./database/posts.json")
 	if err != nil {
-		respondError(w, http.StatusInternalServerError, err.Error())
+		RespondError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 	id := strings.TrimPrefix(r.URL.Path, "/modify/")
 	var posts Posts
 	json.Unmarshal(content, &posts)
-	post := getPostById(w, posts, id)
-	post = getFormData(w, r)
-	if post.Title == "" {
-		respondError(w, http.StatusNoContent, "Not title provided.")
+	post := GetPostById(w, posts, id)
+	RenderHTML(w, "templates/form.html", false, post)
+	if http.MethodPut != r.Method {
+		return
 	}
-	savePost(w, post, "database/posts.json")
+	post = GetFormData(w, r)
+	if post.Title == "" {
+		RespondError(w, http.StatusNoContent, "Not title provided.")
+		return
+	}
+	SavePost(w, post, "database/posts.json")
 }
 func deletePost(w http.ResponseWriter, r *http.Request) {
-	renderHTML(w, "templates/form.html", true)
 	content, err := ioutil.ReadFile("./database/posts.json")
 	if err != nil {
-		respondJSON(w, http.StatusInternalServerError, err.Error())
+		RespondJSON(w, http.StatusInternalServerError, err.Error())
 	}
 	id := strings.TrimPrefix(r.URL.Path, "/delete/")
 	var posts Posts
 	json.Unmarshal(content, &posts)
-	post := getPostById(w, posts, id)
+	post := GetPostById(w, posts, id)
+	RenderHTML(w, "templates/form.html", true, post)
 	for i, p := range posts {
 		if p == post {
 			posts = append(posts[:i], posts[i+1:]...)
 		}
 	}
 	if err := os.Truncate("database/posts.json", 0); err != nil {
-		respondError(w, http.StatusInternalServerError, err.Error())
+		RespondError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 	for _, p := range posts {
-		savePost(w, p, "database/posts.json")
+		SavePost(w, p, "database/posts.json")
 	}
-}
-
-func getPostById(w http.ResponseWriter, posts Posts, id string) Post {
-	for _, post := range posts {
-		if post.PostId == id {
-			respondJSON(w, http.StatusFound, post)
-			return post
-		}
-	}
-	respondError(w, http.StatusNotFound, "Post not found.")
-	return Post{}
-}
-
-func savePost(w http.ResponseWriter, post Post, fileName string) {
-	postByte, _ := json.Marshal(post)
-	if ioutil.WriteFile(fileName, postByte, 0644) != nil {
-		respondError(w, http.StatusInternalServerError, "Error while saving post.")
-	}
-}
-
-func renderHTML(w http.ResponseWriter, htmlTemplate string, readMode bool) {
-	htmlPage, err := template.ParseFiles(htmlTemplate, "templates/header.html", "templates/footer.html")
-	if err != nil {
-		respondError(w, http.StatusInternalServerError, err.Error())
-	}
-	htmlPage.Execute(w, readMode)
-}
-
-func respondError(w http.ResponseWriter, code int, errorMessage string) {
-	respondJSON(w, code, map[string]string{"error": errorMessage})
-	log.Fatal(errorMessage)
-}
-
-func respondJSON(w http.ResponseWriter, code int, data interface{}) {
-	response, _ := json.Marshal(data)
-	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(code)
-	w.Write(response)
 }
---
+++ internal_functions.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+func GetFormData(w http.ResponseWriter, r *http.Request) Post {
+	data := r.PostForm
+	title := data.Get("title")
+	time := time.Now().Second()
+	postId := title + "#" + string(rune(time))
+	return Post{
+		PostId: postId, Time: time, Title: title, Content: data.Get("content"), IsDraft: false,
+	}
+}
+
+func GetPostById(w http.ResponseWriter, posts Posts, id string) Post {
+	for _, post := range posts {
+		if post.PostId == id {
+			RespondJSON(w, http.StatusFound, post)
+			return post
+		}
+	}
+	RespondError(w, http.StatusNotFound, "Post not found.")
+	return Post{}
+}
+
+func SavePost(w http.ResponseWriter, post Post, fileName string) {
+	postByte, _ := json.Marshal(post)
+	if ioutil.WriteFile(fileName, postByte, 0644) != nil {
+		RespondError(w, http.StatusInternalServerError, "Error while saving post.")
+	}
+}
+
+func RenderHTML(w http.ResponseWriter, htmlTemplate string, readMode bool, post Post) {
+	htmlPage, err := template.ParseFiles(htmlTemplate, "templates/header.html", "templates/footer.html")
+	if err != nil {
+		RespondError(w, http.StatusInternalServerError, err.Error())
+	}
+	data := Data{Content: post, ReadMode: readMode}
+	htmlPage.Execute(w, data)
+}
+
+func RespondError(w http.ResponseWriter, code int, errorMessage string) {
+	RespondJSON(w, code, map[string]string{"error": errorMessage})
+	fmt.Println(errorMessage)
+}
+
+func RespondJSON(w http.ResponseWriter, code int, data interface{}) {
+	response, _ := json.Marshal(data)
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
+}

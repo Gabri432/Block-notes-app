be4184fcea25f33e5dbe69ed284d38f6e5c869d1
Gabri432 <gabrielegatti432@gmail.com>
2022-08-12 23:07:08 +0200

    Fixing error of getting wrongly date time of new created post.

--- internal_functions.go
+++ internal_functions.go
@@ -10,12 +10,16 @@ import (
 )
 
 func GetFormData(w http.ResponseWriter, r *http.Request) Post {
+	err := r.ParseForm()
+	if err != nil {
+		RespondError(w, http.StatusNoContent, err.Error())
+	}
 	data := r.PostForm
 	title := data.Get("title")
-	time := time.Now().Second()
-	postId := title + "-" + string(rune(time))
+	time := time.Now()
+	postId := title + "-" + string(rune(time.Second()))
 	return Post{
-		PostId: postId, Time: time, Title: title, Content: data.Get("content"), IsDraft: false,
+		PostId: postId, Time: int(time.Unix()), Title: title, Content: data.Get("content"), IsDraft: false,
 	}
 }
 
@@ -30,7 +34,15 @@ func GetPostById(w http.ResponseWriter, posts Posts, id string) Post {
 }
 
 func SavePost(w http.ResponseWriter, post Post, fileName string) {
-	postByte, _ := json.Marshal(post)
+	content, err := ioutil.ReadFile("database/posts.json")
+	if err != nil {
+		RespondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	var posts Posts
+	json.Unmarshal(content, &posts)
+	posts = append(posts, post)
+	postByte, _ := json.MarshalIndent(posts, "", " ")
 	if ioutil.WriteFile(fileName, postByte, 0644) != nil {
 		RespondError(w, http.StatusInternalServerError, "Error while saving post.")
 	}
